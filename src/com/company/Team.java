e638e8995ca03bca0996e39ab482bfa6074c93fc
ts159 <tommys2056@gmail.com>
2017-02-28 14:25:13 -0500

    has errors

---
+++ src/com/company/Team.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.util.Scanner;
+
+public class Team
+{
+    public Team(int teamNum)
+    {
+        BufferedReader br = new BufferedReader();
+        while(br.)
+    }
+
+    public final String NAME;
+    public final int pointsScored;
+}
