bcdbb8bd37fe4e3b85d8b5ba7a00421a2116a435
ts159 <tommys2056@gmail.com>
2017-03-01 14:04:04 -0500

    added variables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main
 {
     public static void main(String[] args)
     {
-        // test push
+        Team[] teams = new Team[364];
+        for(int i = 1101; i < 1464; i++)
+        {
+            teams[i - 1101] = new Team(i);
+        }
     }
 }
--- src/com/company/Team.java
+++ src/com/company/Team.java
@@ -1,17 +1,29 @@
 package com.company;
 
-import java.io.BufferedReader;
+import java.io.*;
 import java.util.Scanner;
 
 public class Team
 {
     public Team(int teamNum)
     {
-        BufferedReader br = new BufferedReader();
-        while(br.)
+        Scanner sc = new Scanner("RegularSeasonDetailedResults.csv");
+        sc.nextLine();
+        while(sc.hasNextLine())
+        {
+            sc.useDelimiter(",");
+            
+        }
     }
 
     public final String NAME;
-    public final int pointsScored;
-    public final int pointsAgainst;
+    public final double SEASON_WINS; //avg
+    public final double POST_SEASON_WINS; //avg
+    public final double POINTS_PER_GAME;
+    public final double THREE_POINT_PERCENT;
+    public final double FIELD_GOAL_PERCENT;
+    public final double FREE_THROW_PERCENT;
+    public final double TURN_OVER_AVERAGE; //avg turnovers per game
+    public final double STEALS_AVERAGE; //avg steals per game
+    public final double BLOCKS_AVERAGE; //avg blocks per game
 }
