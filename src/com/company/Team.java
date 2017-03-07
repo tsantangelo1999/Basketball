bf68578cd30b29a6266716df80fac8cb114202fd
avalenci <ansmeew@gmail.com>
2017-03-07 14:27:32 -0500

    Added compareto insides

--- src/com/company/Team.java
+++ src/com/company/Team.java
@@ -102,7 +102,51 @@ public class Team// implements Comparable
         return totalPersonalFouls / regularSeasonGames;
     }
 
-
+    public static double compareTo(Team a, Team b)
+    {
+        int points[] = new int[2];
+        if (a.getSeasonWinrate() > b.getSeasonWinrate())
+            points[0]++;
+        else
+        points[1]++;
+        if (a.getPointsPerGame() > b.getPointsPerGame())
+            points[0]++;
+        else
+        points[1]++;
+        if (a.getPointsAgainstPerGame() < b.getPointsAgainstPerGame())
+            points[0]++;
+        else
+        points[1]++;
+        if (a.getThreePointPercent() > b.getThreePointPercent())
+            points[0]++;
+        else
+        points[1]++;
+        if (a.getFieldGoalPercent() > b.getFieldGoalPercent())
+            points[0]++;
+        else
+        points[1]++;
+        if (a.getFreeThrowPercent() > b.getFreeThrowPercent())
+            points[0]++;
+        else
+        points[1]++;
+        if (a.getTurnoversPerGame() < b.getTurnoversPerGame())
+            points[0]++;
+        else
+        points[1]++;
+        if (a.getStealsPerGame() > b.getStealsPerGame())
+            points[0]++;
+        else
+        points[1]++;
+        if (a.getBlocksPerGame() > b.getBlocksPerGame())
+            points[0]++;
+        else
+        points[1]++;
+        if (a.getPersonalFoulsPerGame() > b.getPersonalFoulsPerGame())
+            points[0]++;
+        else
+        points[1]++;
+        return points[0]/10;
+    }
 
     public double tourneyTotalPoints;
     public double tourneyTotalPointsAgainst;
@@ -263,10 +307,4 @@ public class Team// implements Comparable
         return 0;
     }
 
-
-    //winrate, points per game, points against per game, three point%, field goal%, free throw %, turnovers per game, steals per game, blocks per game, personal fouls per game
-    //public static int compareTo(Team a, Team b)
-    {
-
-    }
 }
